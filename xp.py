13da59346e185afa2b34136914a1abdd8761c799
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-12 18:45:03 -0700

    add entry point at a higher level

---
+++ xp.py
@@ -0,0 +1,9 @@
+import sys
+
+# noinspection PyUnresolvedReferences
+import xappt_plugins.plugins
+import xappt_qt.browser
+
+
+if __name__ == '__main__':
+    sys.exit(xappt_qt.browser.entry_point())
