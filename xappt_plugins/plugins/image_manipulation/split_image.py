4a1242222e271b1b1e4a21bd4b66deef1360ec87
Christopher Montesano <c.e.montesano@gmail.com>
2020-11-17 14:37:27 -0500

    more options for split tool

--- xappt_plugins/plugins/image_manipulation/split_image.py
+++ xappt_plugins/plugins/image_manipulation/split_image.py
@@ -27,6 +27,10 @@ class SplitImage(xappt.BaseTool):
                           description="How many rows of tiles are there?", default=8)
     replace = xappt.ParamBool(options={'short_name': "r"}, default=False,
                               description="Should we replace existing files?")
+    start = xappt.ParamInt(options={'short_name': "s"}, minimum=0,
+                           description="What number should the counting start at?", default=1)
+    naming = xappt.ParamString(options={'short_name': "n"},
+                               description="What name format should be used?", default="{name}.{index:03d}")
 
     @classmethod
     def name(cls) -> str:
@@ -42,11 +46,10 @@ class SplitImage(xappt.BaseTool):
 
     def execute(self, **kwargs) -> int:
         input_path = self.input_image.value
-        output_name, output_ext = os.path.splitext(os.path.basename(input_path))
-        output_path = os.path.join(self.output_path.value, f'{output_name}.%03d{output_ext}')
+        name, ext = os.path.splitext(os.path.basename(input_path))
 
-        if output_ext.lower() not in SUPPORTED_EXTENSIONS.keys():
-            self.interface.error(f"File extension '{output_ext}' is not supported")
+        if ext.lower() not in SUPPORTED_EXTENSIONS.keys():
+            self.interface.error(f"File extension '{ext}' is not supported")
             return 1
 
         cols = self.columns.value
@@ -57,16 +60,17 @@ class SplitImage(xappt.BaseTool):
 
         tile_size = (sw // cols, sh // rows)
 
-        mode = SUPPORTED_EXTENSIONS[output_ext.lower()]['mode']
+        mode = SUPPORTED_EXTENSIONS[ext.lower()]['mode']
 
         total = rows * cols
         self.interface.progress_start()
 
         for y in range(rows):
             for x in range(cols):
-                tile_index = ((y * cols) + x) + 1
+                tile_index = ((y * cols) + x) + self.start.value
                 self.interface.progress_update(f"Extracting tile {tile_index}", tile_index / total)
-                dst = output_path % tile_index
+                file_name = self.naming.value.format(name=name, index=tile_index)
+                dst = os.path.join(self.output_path.value, f'{file_name}{ext}')
                 result = Image.new(mode, tile_size)
                 result.paste(img, (-x * tile_size[0], -y * tile_size[1]))
                 result.save(dst)
